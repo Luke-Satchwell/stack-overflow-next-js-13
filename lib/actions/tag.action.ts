0ea391cf5462113fcc924dd0992b4df8e8494e56
Luke Satchwell <satch744@googlemail.com>
2023-10-26 17:21:07 +0100

    implement tags page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteracted(params: GetTopInteractedTagsParams) {
   try {
@@ -24,6 +25,21 @@ export async function getTopInteracted(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag" },
       { _id: "3", name: "tag" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    // get tags
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
