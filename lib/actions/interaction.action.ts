510ac964cb332bf341a88643e60addb67630d186
Luke Satchwell <satch744@googlemail.com>
2023-10-28 16:06:08 +0100

    rename interaction files for view count

--- lib/actions/interaction.action.ts
+++ lib/actions/interaction.action.ts
@@ -3,7 +3,7 @@
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import { ViewQuestionParams } from "./shared.types";
-import Interaction from "@/database/interaction.action";
+import Interaction from "@/database/interaction.model";
 
 export async function viewQuestion(params: ViewQuestionParams) {
   try {
